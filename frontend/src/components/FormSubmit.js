e4fe3bac2250a790cf8a2a0d408c295d488627eb
Marie Treschow <marie.treschow@mobgen.com>
2018-06-14 18:45:47 +0200

    adding form labels

--- frontend/src/components/FormSubmit.js
+++ frontend/src/components/FormSubmit.js
@@ -19,23 +19,30 @@ export default class RegisterForm extends React.Component {
     })
     .then((response) => {
       console.log(response);
+      //handle response save timestamp to render user from database and render other component
+
     })
     .catch((error) => {
       console.log(error);
     });
 
   }
+  //add error information if provided wrong country etc, add dropdown for country?
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <form className="col-6 offset-3 pt-3" onSubmit={this.handleFormSubmit}>
+            <label for="username">Username</label>
             <div className="form-group">
               <input className="form-control" type="text" name="username" />
             </div>
+            <label for="company">Company</label>
             <div className="form-group">
               <input className="form-control" type="text" name="company" />
             </div>
+            <label for="Country">Country</label>
             <div className="form-group">
               <input className="form-control" type="text" name="country" />
             </div>
