8b85e306209d8e78c180cc352949b6b27ae15131
Marie Treschow <marie.treschow@mobgen.com>
2018-06-16 13:21:06 +0200

    removing packaged not used

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FormSubmit from '../components/FormSubmit';
 import RegisteredUser from '../components/RegisteredUser';
